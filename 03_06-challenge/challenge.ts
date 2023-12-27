c46660cedeb58961af7c5d387857483008845040
PaulThorbergsson <132074766+PaulThorbergsson@users.noreply.github.com>
2023-12-27 16:37:19 +0000

    fixed ommited ;

--- 03_06-challenge/challenge.ts
+++ 03_06-challenge/challenge.ts
@@ -1,5 +1,5 @@
 export function Challenge() {
-  type MyType = String | boolean | Array<String | number>
+  type MyType = String | boolean | Array<String | number>;
   let foo: MyType = "Hello!";
   foo = false;
   foo = ["test", 100];
